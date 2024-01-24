6f36130c198c53b30143d97ba0bf6f4ab80d1092
shintake98765 <shintake.98765@gmail.com>
2024-01-24 11:28:25 +0900

    develop app1 commit

--- src/app/demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/app/demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -25,7 +25,7 @@ class Demo1ApplicationTests {
 		
 		ObjectMapper objectMapper = new ObjectMapper();
 		
-		String a = "e";
+		String a = "f";
 		
 		DemoDto demoDto = new DemoDto();
 		demoDto.setId(100);
