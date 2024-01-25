a19e2ef48fc26abad650e08dcd4e6a53ff9afe65
shintake98765 <shintake.98765@gmail.com>
2024-01-26 08:48:12 +0900

    ct57

--- src/app/demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/app/demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -25,7 +25,7 @@ class Demo1ApplicationTests {
 		
 		ObjectMapper objectMapper = new ObjectMapper();
 		
-		String a = "23";
+		String a = "24";
 		
 		DemoDto demoDto = new DemoDto();
 		demoDto.setId(100);
