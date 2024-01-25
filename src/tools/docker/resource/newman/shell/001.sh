85abe39d8a8b9de91f851905cd1150ad988073be
shintake98765 <shintake.98765@gmail.com>
2024-01-25 11:28:40 +0900

    ct42

--- src/tools/docker/resource/newman/shell/001.sh
+++ src/tools/docker/resource/newman/shell/001.sh
@@ -3,6 +3,6 @@ collection="postman_collection.json"
 testcase=test001
 newman run /newman/shell/${collection} --verbose -r cli,junit --reporter-junit-export /newman/logs/${testcase}.xml > /newman/logs/${testcase}.log
 if [ $? -ne 0 ]; then
-  echo ${testcase} >> /newman/logs/error.log
+#  echo ${testcase} >> /newman/logs/error.log
 fi
 exit $?
